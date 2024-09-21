6e7b6c7a4ccb29a7eac823845ed3242fc9b76bb5
Ronaldo <ronaldosdias.1212@gmail.com>
2024-09-20 21:35:09 -0300

    enemy3 going up and down

--- code/Player.py
+++ code/Player.py
@@ -29,7 +29,7 @@ class Player(Entity):
         self.shot_delay -= 1
         if self.shot_delay == 0:
             self.shot_delay = ENTITY_SHOT_DELAY[self.name]
-            pressed_key = pygame.key.get_pressed()
+             pressed_key = pygame.key.get_pressed()
             if pressed_key[PLAYER_KEY_SHOOT[self.name]]:
                 return PlayerShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
             else:
