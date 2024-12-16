94e4cfc92388606f5063f3b8989daf780453a37d
milacamera <85769498+milacamera@users.noreply.github.com>
2024-12-16 20:42:45 -0300

    Moving Enemy3 in Enemy.py

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
-
 
 class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
@@ -18,3 +17,17 @@ class Enemy(Entity):
         if self.shot_delay == 0:
             self.shot_delay = ENTITY_SHOT_DELAY[self.name]
             return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+
+class Enemy3(Enemy):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.direction = 1  # 1 para descer, -1 para subir
+
+    def move(self):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        self.rect.centery += self.direction * ENTITY_SPEED[self.name]
+
+        if self.rect.top <= 0:
+            self.direction = 1
+        elif self.rect.bottom >= WIN_HEIGHT:
+            self.direction = -1
