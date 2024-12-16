089643ef7db776ce4c29b19eded73d76bc2df81e
milacamera <85769498+milacamera@users.noreply.github.com>
2024-12-16 20:40:43 -0300

    Creating Bg3 and Enemy3 in EntityFactory.py

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -7,7 +7,6 @@ from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Enemy import Enemy
 from code.Player import Player
 
-
 class EntityFactory:
 
     @staticmethod
@@ -25,6 +24,12 @@ class EntityFactory:
                     list_bg.append(Background(f'Level2Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level2Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Level3Bg':  # Novo background para o level 3
+                list_bg = []
+                for i in range(5):  # level3bg images number
+                    list_bg.append(Background(f'Level3Bg{i}', (0, 0)))
+                    list_bg.append(Background(f'Level3Bg{i}', (WIN_WIDTH, 0)))
+                return list_bg
             case 'Player1':
                 return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
             case 'Player2':
@@ -33,3 +38,5 @@ class EntityFactory:
                 return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
             case 'Enemy2':
                 return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+            case 'Enemy3':  # Novo inimigo para o level 3
+                return Enemy('Enemy3', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
