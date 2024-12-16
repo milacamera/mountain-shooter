3178e76ff18929815480e3a02e289d3787d86775
milacamera <85769498+milacamera@users.noreply.github.com>
2024-12-16 20:44:54 -0300

    Duration and enemy criation for level3 in Level.py

--- code/Level.py
+++ code/Level.py
@@ -15,7 +15,6 @@ from code.EntityFactory import EntityFactory
 from code.EntityMediator import EntityMediator
 from code.Player import Player
 
-
 class Level:
     def __init__(self, window: Surface, name: str, game_mode: str, player_score: list[int]):
         self.timeout = TIMEOUT_LEVEL
@@ -57,7 +56,7 @@ class Level:
                     pygame.quit()
                     sys.exit()
                 if event.type == EVENT_ENEMY:
-                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    choice = random.choice(('Enemy1', 'Enemy2', 'Enemy3'))  # Adicionado Enemy3
                     self.entity_list.append(EntityFactory.get_entity(choice))
                 if event.type == EVENT_TIMEOUT:
                     self.timeout -= TIMEOUT_STEP
